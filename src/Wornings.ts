5328efa6c3a83cc9c775c23a9c5d382bab42c017
saleksanyan <92786852+saleksanyan@users.noreply.github.com>
2024-05-31 11:43:57 +0400

    Changed spaces in wornings.ts

--- src/Wornings.ts
+++ src/Wornings.ts
@@ -1,21 +1,8 @@
 class Wornings{
-
-
-
     public static notValidMove(){console.log("\nThis is not a valid move!")}
-
-
     public static notValidFigure(){console.log("\nNot valid figure for your move!")}
-
-
     public static notValidPosition(){console.log("\nNot valid position!")}
-
-
     public static notValidHistoryReference(){console.log("\nNot valid history reference!")}
-
-
-
-
 }
 
 
